e6aa8c6d06dc049bf61b032377a605bd28ede5ba
Muhammad Aadil Ghani <muhammad.ghani.3@city.ac.uk>
2025-05-31 21:36:50 +0100

    update

--- next.config.js
+++ next.config.js
@@ -1,26 +1,31 @@
-const path = require('path')
- 
-module.exports = {
+const path = require("path");
+
+const nextConfig = {
+  output: "export", // This makes it a static site
+  distDir: "dist", // Missing comma was here
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
   images: {
+    unoptimized: true, // Required for static export
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
       },
       {
-        protocol: 'https',
-        hostname: 'media.dev.to',
-        pathname: '**',
+        protocol: "https",
+        hostname: "media.dev.to",
+        pathname: "**",
       },
       {
-        protocol: 'https',
-        hostname: 'media2.dev.to',
-        pathname: '**',
+        protocol: "https",
+        hostname: "media2.dev.to",
+        pathname: "**",
       },
     ],
   },
-}
+};
+
+module.exports = nextConfig;
