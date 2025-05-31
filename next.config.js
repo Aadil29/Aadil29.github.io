f52f352561845c6271cef8ba002819542d873f44
Muhammad Aadil Ghani <muhammad.ghani.3@city.ac.uk>
2025-05-31 22:31:16 +0100

    2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 
 const nextConfig = {
+  basePath: "/Aadil29.github.io",
   output: "export", // This makes it a static site
   distDir: "dist", // Missing comma was here
   sassOptions: {
